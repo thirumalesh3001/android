14db6c634b0f5ed243dfb427b9fe3994c2805711
santhu <thirumalesh3001@gmail.com>
2017-11-30 23:43:39 -0800

    Initial commit

---
+++ assets/www/js/index.js
@@ -0,0 +1,13 @@
+
+
+function addNumbers()
+                {
+                        var val1 =10;
+                        var val2 = 20;
+                        var ansD = val1+val2;
+                        alert('answer'+ansD);
+
+                }
+
+
+
